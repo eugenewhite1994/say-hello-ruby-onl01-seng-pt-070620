6cc3c06979ca42ef55b4f14b16d7d55b432cb22e
Eugene White <eugenewhite1994@gmail.com>
2020-07-06 23:45:46 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,11 @@
 # Build your say_hello method here
 
-def say_hello
-  puts "Hello, Ruby Programmer!"
+def say_hello(name)
+  puts "Hello #{name}"
 end
 
-def say_hello(name)
-  puts "Hello #{name}!"
+def say_hello
+  puts "Hello Ruby Programmer!"
 end
 
 say_hello("Eugene")
